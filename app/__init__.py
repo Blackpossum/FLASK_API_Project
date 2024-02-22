65fa606224ee663d4386a032106955a37eb15ea8
Blackpossum <yosaphatharwindra@gmail.com>
2024-02-22 22:22:24 +0700

    db migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from app.routes import Animal_management,Employe_management,Feeding_management
 import os
-from app.utils.database import db
+from app.utils.database import db, migrate
+from app.models import animals,employee,feeding
 
 app = Flask(__name__)
 
@@ -13,8 +14,9 @@ DATABASE_HOST = os.getenv('DATABASE_HOST')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 DATABASE_PORT = os.getenv('DATABASE_PORT')
 app.config["SQLALCHEMY_DATABASE_URI"] = f"{DATABASE_TYPE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-db.init_app(app)
 
+db.init_app(app)
+migrate.init_app(app, db)
 
 @app.route("/")
 def home():
